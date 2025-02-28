cebc78bc4bf6f108b99ccc570e6898ba6c2c22a1
400hoops <studstubstubyshub@proton.me>
2025-02-27 20:15:48 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -20,7 +20,6 @@ rc-update add docker default
 
 # Create a new group and user for Samba
 echo "Creating a new group and user for Samba..."
-# Create a new group for Samba if it doesn't exist
 if ! getent group samba_group > /dev/null; then
   echo "Creating a new group for Samba..."
   addgroup samba_group
@@ -31,11 +30,11 @@ read -p "Enter a username: " USERNAME
 
 # Create a new system user
 echo "Creating a new system user..."
-adduser -S $USERNAME -G samba_group
+adduser -S "$USERNAME" -G samba_group
 
 # Set a smbpasswd for the new user
 echo "Setting a smbpasswd for the new user..."
-smbpasswd -a $USERNAME
+smbpasswd -a "$USERNAME"
 
 # Get the pool name from the user
 read -p "Enter a pool name: " POOL_NAME
@@ -58,26 +57,30 @@ fi
 mkdir -p /zfs
 
 # Generate a random key for the ZFS pool
-openssl rand -out /zfs/$POOL_NAME.key -rand /dev/urandom 32
-chmod 600 /zfs/$POOL_NAME.key
+openssl rand -out /zfs/"$POOL_NAME".key -rand /dev/urandom 32
+chmod 600 /zfs/"$POOL_NAME".key
 
 # Create the ZFS pool with encryption
-zpool create -f -o ashift=12 -O encryption=aes-256-ccm -O keylocation=file:///zfs/$POOL_NAME.key -O keyformat=raw $POOL_NAME mirror $DRIVE_ID_1 $DRIVE_ID_2
+zpool create -f -o ashift=12 -O encryption=aes-256-ccm -O keylocation=file:///zfs/"$POOL_NAME".key -O keyformat=raw "$POOL_NAME" mirror "$DRIVE_ID_1" "$DRIVE_ID_2"
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to create ZFS pool."
+  exit 1
+fi
 
 # Create a ZFS dataset for Immich
-zfs create $POOL_NAME/immich
-mkdir -p /$POOL_NAME/immich/postgres
-mkdir -p /$POOL_NAME/immich/library
-chmod 770 /$POOL_NAME/immich/postgres
-chmod 770 /$POOL_NAME/immich/library
-chown :docker /$POOL_NAME/immich
-chown :docker /$POOL_NAME/immich/postgres
-chown :docker /$POOL_NAME/immich/library
+zfs create "$POOL_NAME"/immich
+mkdir -p /"$POOL_NAME"/immich/postgres
+mkdir -p /"$POOL_NAME"/immich/library
+chmod 770 /"$POOL_NAME"/immich/postgres
+chmod 770 /"$POOL_NAME"/immich/library
+chown :docker /"$POOL_NAME"/immich
+chown :docker /"$POOL_NAME"/immich/postgres
+chown :docker /"$POOL_NAME"/immich/library
 
 # Create a ZFS dataset for Time Machine
-zfs create $POOL_NAME/time_machine
-chmod 770 /$POOL_NAME/time_machine
-chown :samba_group /$POOL_NAME/time_machine
+zfs create "$POOL_NAME"/time_machine
+chmod 770 /"$POOL_NAME"/time_machine
+chown :samba_group /"$POOL_NAME"/time_machine
 
 # Configure Samba
 echo "Configuring Samba..."
@@ -88,32 +91,17 @@ server role = standalone
 
 # Global parameters
 [global]
-  # Bind to all available network interfaces
   bind interfaces only = yes
-
-  # Set the minimum and maximum SMB protocol versions
   client ipc min protocol = SMB3
   client ipc signing = required
   client signing = required
   server min protocol = SMB3
   server signing = required
-
-  # Disable NetBIOS over TCP/IP
   disable netbios = yes
-
-  # Map unknown users to the guest account
   map to guest = Bad User
-
-  # Restrict anonymous access
   restrict anonymous = 2
-
-  # Set the security mode to user-level authentication
   security = USER
-
-  # Optimize socket options for performance
   socket options = TCP_NODELAY IPTOS_LOWDELAY
-
-  # Enable Fruit metadata streaming for macOS compatibility
   fruit:metadata = stream
   fruit:model = MacSamba
   fruit:veto_appledouble = no
@@ -121,55 +109,33 @@ server role = standalone
   fruit:wipe_intentionally_left_blank_rfork = yes
   fruit:delete_empty_adfiles = yes
   fruit:posix_rename = yes
-
-  # Use the TDB backend for ID mapping
   idmap config * : backend = tdb
-
-  # Disable browsing of shares
   browseable = no
-
-  # Enable VFS objects for Fruit and streams_xattr
   vfs objects = fruit streams_xattr
 
-# Time Machine share
 [Time Machine]
-  # Set the comment for the share
   comment = Time Machine Backup
-
-  # Set the permissions for the share
   force create mode = 0660
   force directory mode = 0770
   force group = samba_group
   force user = nobody
-
-  # Enable ACL inheritance
   inherit acls = yes
-
-  # Set the path to the Time Machine backup directory
-  path = /$POOL_NAME/time_machine
-
-  # Allow read and write access to the share
+  path = /"$POOL_NAME"/time_machine
   read only = no
-
-  # Enable Time Machine support
   fruit:time machine = yes
 EOF
 
 # Configure Docker
-
-# Get the docker-compose file from github/immich-app/immich
 wget -O docker-compose.yml https://github.com/immich-app/immich/releases/latest/download/docker-compose.yml
 
 DB_PASSWORD=$(openssl rand -base64 12)
 
-# Create the .env file in the user's home directory
-
 cat > ~/.env <<EOF
-UPLOAD_LOCATION=/$POOL_NAME/immich/library
-DB_DATA_LOCATION=/$POOL_NAME/immich/postgres
+UPLOAD_LOCATION=/"$POOL_NAME"/immich/library
+DB_DATA_LOCATION=/"$POOL_NAME"/immich/postgres
 DB_USERNAME=postgres
 DB_DATABASE_NAME=immich
-DB_PASSWORD=e0f
+DB_PASSWORD=$DB_PASSWORD
 IMMICH_VERSION=release
 EOF
 
@@ -182,7 +148,7 @@ if [ -z "$PORT_NUMBER" ]; then
   exit 1
 fi
 
-if ! [[ $PORT_NUMBER =~ ^[0-9]+$ ]] || [ $PORT_NUMBER -lt 1 ] || [ $PORT_NUMBER -gt 65535 ]; then
+if ! [[ "$PORT_NUMBER" =~ ^[0-9]+$ ]] || [ "$PORT_NUMBER" -lt 1 ] || [ "$PORT_NUMBER" -gt 65535 ]; then
   echo "Error: Invalid port number. Please enter a number between 1 and 65535."
   exit 1
 fi
@@ -205,22 +171,13 @@ cat > /etc/iptables/rules-save <<EOF
 :FORWARD DROP [0:0]
 :OUTPUT DROP [0:0]
 
-# Allow loopback traffic
 -A INPUT -i lo -j ACCEPT
 -A OUTPUT -o lo -j ACCEPT
-
-# Allow RELATED and ESTABLISHED traffic
 -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-
-# Allow SSH from local network
--A INPUT -p tcp --dport 22 -j ACCEPT
-
-# Allow incoming Samba connections from local network
+-A INPUT -p tcp --dport $PORT_NUMBER -j ACCEPT
 -A INPUT -p tcp --dport 445 -j ACCEPT
-
-# Allow outgoing NTP
--A OUTPUT -p udp
+-A OUTPUT -p udp -j ACCEPT
 EOF
 iptables-save > /etc/iptables/rules-save
 
@@ -230,12 +187,13 @@ rc-service networking restart
 echo "Configuring crontab..."
 crontab -l | { 
   cat; 
-  echo "0       3       *       *       *       apk update && apk upgrade"; 
-  echo "30     3       *       *       *       docker compose pull && docker compose up -d && docker image prune -af"; 
+  echo "0 3 * * * apk update && apk upgrade"; 
+  echo "30 3 * * * docker compose pull && docker compose up -d && docker image prune -af"; 
 } | crontab -
 
-  chmod 700 /root
-  chmod 600 /boot/grub/grub.cfg
-  chmod 600 /etc/ssh/sshd_config
+chmod 700 /root
+chmod 600 /boot/grub/grub.cfg
+chmod 600 /etc/ssh/sshd_config
+chmod 600 ~/.env
 
 echo "Done"
